f7cb3b827480608a753f563b1282ca5f58d50dd4
Likhitha <likhithaboyina03@gmail.com>
2023-07-14 21:27:19 +0530

    DAY-6

---
+++ pattern26.py
@@ -0,0 +1,16 @@
+# Another diamond pattern of star
+#     *
+#    * *
+#   *   *
+#  *     *
+# *       *
+#  *     *
+#   *   *
+#    * *
+#     *
+
+rows=int(input())              
+for i in range(rows+1):
+    print(" " * (rows - i) + "*" + " " * ((2*i)-1) + "*" * (i != 0))
+for i in range(rows-1 , -1, -1):
+    print(" " * (rows - i) + "*" + " " * ((2*i)-1) + "*" * (i != 0))
